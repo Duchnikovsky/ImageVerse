2e150e1c9e6608e67c5ebcab0775e19c5400e058
Duchnikovsky <filipduchnik@outlook.com>
2023-08-08 15:07:35 +0200

    Sidebar hotfix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -60,7 +60,7 @@ export default async function Sidebar({ session }: SidebarProps) {
         },
       ],
     },
-  })
+  });
 
   return (
     <div className={CSS.sidebar}>
@@ -84,34 +84,38 @@ export default async function Sidebar({ session }: SidebarProps) {
               </Button>
             </Link>
           </div>
-          {proposedUsersCount > 0 && <div className={CSS.proposedUsers}>
-            <div className={CSS.proposedDescription}>Suggestions for you</div>
-            {proposedUsers.map((user, index) => (
-              <SuggestedUser user={user} key={index}/>
-            ))}
-          </div>}
+          {proposedUsersCount > 0 && (
+            <div className={CSS.proposedUsers}>
+              <div className={CSS.proposedDescription}>Suggestions for you</div>
+              {proposedUsers.map((user, index) => (
+                <SuggestedUser user={user} key={index} />
+              ))}
+            </div>
+          )}
         </div>
       ) : (
         <div className={CSS.rightPanel}>
-          <div className={CSS.nameBox}>
-            <p className={CSS.name}>Welcome to the ImageVerse</p>
+          <div className={CSS.userBox}>
+            <div className={CSS.nameBox}>
+              <p className={CSS.name}>Welcome to the ImageVerse</p>
+            </div>
+            <div className={CSS.description}>
+              Sign in and share your experiences with friends and followers by
+              adding new posts
+            </div>
+            <Link href="/signIn">
+              <Button
+                width="80%"
+                height="2rem"
+                fontSize="17px"
+                isDisabled={false}
+                isLoading={false}
+                margin={"auto"}
+              >
+                Sign in to ImageVerse
+              </Button>
+            </Link>
           </div>
-          <div className={CSS.description}>
-            Sign in and share your experiences with friends and followers by
-            adding new posts
-          </div>
-          <Link href="/signIn">
-            <Button
-              width="80%"
-              height="2rem"
-              fontSize="17px"
-              isDisabled={false}
-              isLoading={false}
-              margin={"auto"}
-            >
-              Sign in to ImageVerse
-            </Button>
-          </Link>
         </div>
       )}
     </div>
