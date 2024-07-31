f373eef57c3af6ec5aa0c75ff044317d880614fb
Duchnikovsky <filipduchnik@outlook.com>
2024-07-31 15:56:38 +0200

    feat(next): Update version of nextjs and next-auth

--- next.config.js
+++ next.config.js
@@ -21,9 +21,6 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  experimental: {
-    serverActions: true,
-  },
 }
 
 module.exports = nextConfig
